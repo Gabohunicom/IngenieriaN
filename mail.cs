b4d7bdb57a9f3995cbb887363d7ebb652bd802d1
Gabohosorio <100605118+Gabohosorio@users.noreply.github.com>
2024-04-26 21:05:15 -0500

    Add files via upload

---
+++ mail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+
+namespace Prueba
+{
+  public class mail
+  { public string Enviar(string Para, string Asunto, String Texto)
+    { string Desde = "tucorreoelectronico@gmail.com";
+      try
+      { MailMessage correo = new MailMessage();
+        correo.From = new MailAddress(Desde, "Verification code");
+        correo.To.Add(new MailAddress(Para));
+        correo.Subject = Asunto;
+        correo.Body = Texto;
+        correo.IsBodyHtml = false;
+
+        SmtpClient Envio = new SmtpClient("smtp.gmail.com", 587);
+        Envio.Credentials = new NetworkCredential(Desde, "tucontraseña");
+        Envio.DeliveryMethod = SmtpDeliveryMethod.Network;
+        Envio.EnableSsl = true;
+        //Envio.UseDefaultCredentials = false;
+
+        Envio.Send(correo);
+        return "Ok";
+      }
+      catch(Exception er)
+      { return er.Message.ToString(); }
+    }
+  }
+}
